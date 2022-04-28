5a9af1e64fa1a415b4fbbafd01b2dc480045618f
patricioo1 <patryksmolarz1@gmail.com>
2022-04-28 22:40:57 +0200

    new style css

--- js/index.js
+++ js/index.js
@@ -18,7 +18,6 @@ const book = {
     category: selectCategory.value
 }
 
-
 inputTitle.addEventListener('input', (e) => {
     book.title = e.target.value;
     errorTitle.classList.remove('show');
@@ -39,50 +38,68 @@ selectCategory.addEventListener('change', (e) => {
     errorCategory.classList.remove('show');
 })
 
+const removeBook = (book) => {
+    const library = JSON.parse(localStorage.getItem('library'));
+    const filtered = library.filter(item => item.title !== book.title);
+    return localStorage.setItem('library', JSON.stringify(filtered));
+}
+
 const addToLibrary = () => {
-    let libraryBook = document.createElement('div');
+    const deleteBook = document.createElement('button');
+    deleteBook.className = 'delete-book';
+    const libraryBook = document.createElement('div');
     libraryBook.className = 'book-library_books';
     libraryWrapper.appendChild(libraryBook);
     books = JSON.parse(localStorage.getItem('library')) || [];
     books.push(book);
-    localStorage.setItem('library', JSON.stringify(books))
-    console.log(books);
+    localStorage.setItem('library', JSON.stringify(books));
     Object.keys(book).map(key => {
-        libraryBook.innerHTML += `<p>${book[key]}</p>`
+        libraryBook.innerHTML += `<p>${book[key]}</p>`;
     });
+    libraryBook.appendChild(deleteBook);
+    deleteBook.addEventListener('click', () => {
+        libraryWrapper.removeChild(libraryBook);
+        removeBook(book);
+    })
 }
 
 const dataFromLocalStorage = () => {
     const dataLocalStorage = JSON.parse(localStorage.getItem('library'));
-    if (dataLocalStorage.length > 0) {
+    if (dataLocalStorage) {
         dataLocalStorage.map(book => {
-            let libraryBook = document.createElement('div');
+            const libraryBook = document.createElement('div');
+            const deleteBook = document.createElement('button');
             libraryBook.className = 'book-library_books';
+            deleteBook.className = 'delete-book';
             libraryWrapper.appendChild(libraryBook);
             Object.keys(book).map(key => {
-                libraryBook.innerHTML += `<p>${book[key]}</p>`
+                libraryBook.innerHTML += `<p>${book[key]}</p>`;
+            })
+            libraryBook.appendChild(deleteBook);
+            deleteBook.addEventListener('click', () => {
+                removeBook(book);
+                libraryWrapper.removeChild(libraryBook);
             })
         })
     }
 }
 
-window.addEventListener('load', dataFromLocalStorage);
-
 const validateForm = () => {
     if (inputTitle.value.length < 1) {
-        errorTitle.classList.add('show')
+        errorTitle.classList.add('show');
     } else if (inputAuthor.value.length < 3) {
-        errorAuthor.classList.add('show')
+        errorAuthor.classList.add('show');
     } else if (inputPriority.value === '' || inputPriority.value > 5 || inputPriority.value <= 0) {
-        errorPriority.classList.add('show')
+        errorPriority.classList.add('show');
     } else if (selectCategory.value === 'choose') {
-        errorCategory.classList.add('show')
+        errorCategory.classList.add('show');
     } else {
         addToLibrary();
         inputTitle.value = '';
         inputAuthor.value = '';
         inputPriority.value = '';
-        selectCategory.value = '';
+        selectCategory.value = 'choose';
+        window.location.reload();
     }
 }
 
@@ -93,3 +110,4 @@ const addBook = (e) => {
 
 addButton.addEventListener('click', addBook);
 
+window.addEventListener('load', dataFromLocalStorage);
